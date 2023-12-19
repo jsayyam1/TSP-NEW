cf7169983dfa3ed29f795ea38f3449f2823d2b46
poonawala2303 <mustafa.poonawala82@gmail.com>
2023-12-19 22:17:56 +0530

    Footer modified and added new one

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,16 +1,12 @@
-import styled from '@emotion/styled'
-import { Box, Card, ListItem,  ListItemText, Stack } from '@mui/material'
+import { Button , Grid, Input , Typography } from '@mui/material'
 import React from 'react'
-
-// ListItemText = styled(ListItemText)({
-//     textAlign : 'center'
-// })
-
+import TSP from '../Assets/TSP Logo.jpg'
+import { Mail } from '@mui/icons-material'
 
 const Footer = () => {
   return (
     <>
-        <Box>
+        {/* <Box>
             <Card sx={{bgcolor: 'black' , position: 'relative' , top: '250px'}}>
             
             <Stack direction="row" spacing={9}>
@@ -78,7 +74,48 @@ const Footer = () => {
             </Stack>
 
             </Card>
-        </Box>  
+        </Box>   */}
+
+        {/* <Container sx={{position: 'relative' , top: '280px' ,color: 'white'}} > */}
+            <Grid bgcolor={'#323131'} container spacing={3} sx={{ width: '100%' , height: {xs: '800px'  , sm:'300px'} , position: 'relative' , top: '280px' ,color: 'white'}}>
+
+                <Grid display={'flex'} margin={'50px 10px'} justifyContent={'center'} item xs={12} sm={6} md={4} lg={3} >
+                    <img src={TSP} alt='' width={'150px'} height={'150px'} style={{borderRadius: '50%'}}></img>
+                </Grid>
+
+                <Grid item >
+                    <Typography variant='h4' sx={{textAlign: 'center',margin: '15px' , paddingBottom: '20px '}}>Home</Typography>
+                    <Typography variant='h6' sx={{textAlign: 'center' , fontSize: '12px' , fontWeight: 'bold', margin: '15px' , paddingBottom: '10px'}} >About Us</Typography>
+                    <Typography variant='h6' sx={{textAlign: 'center' , fontSize: '12px' , fontWeight: 'bold', margin: '15px' , paddingBottom: '10px'}} >Why Choose us</Typography>
+                    <Typography variant='h6' sx={{textAlign: 'center' , fontSize: '12px' , fontWeight: 'bold', margin: '15px' , paddingBottom: '10px'}} >Pricing</Typography>
+                </Grid>
+
+                <Grid item>
+                    <Typography variant='h4' sx={{textAlign: 'center',margin: '15px' , paddingBottom: '20px '}}>Resources</Typography>
+                    <Typography variant='h6' sx={{textAlign: 'center' , fontSize: '12px' , fontWeight: 'bold' , margin: '15px' , paddingBottom: '10px'}} >Privacy Policy</Typography>
+                    <Typography variant='h6' sx={{textAlign: 'center' , fontSize: '12px' , fontWeight: 'bold' , margin: '15px' , paddingBottom: '10px'}} >Terms and Conditions</Typography>
+                    <Typography variant='h6' sx={{textAlign: 'center' , fontSize: '12px' , fontWeight: 'bold' , margin: '15px' , paddingBottom: '10px'}} >Contact Us</Typography>
+                </Grid> 
+
+                <Grid sx={{marginLeft: {xs: '0px' , sm: '60px'}}} item>
+                    <Typography variant='h4' sx={{textAlign: 'center',margin: '15px'}}>The Stallion Project</Typography>
+                    <Typography variant='h6' sx={{margin: '15px 18px'}}>Get in touch with us</Typography>
+                </Grid> 
+
+                <Grid sx={{marginLeft: {xs: '0px' , sm: '60px'}}} item>
+                    <Input sx={{position:'relative' , top: {xs : -32, sm: '-150px'} , left: {xs: 18 , sm:779} , bgcolor: 'white' , borderRadius: '10px' , height: '55px' , width: {xs:'290px' , sm:'300px'} , padding: '10px'}}  
+             startAdornment = {<Mail sx={{margin: '5px'}}/>} endAdornment={<Button sx={{bgcolor: '#0b151d' , color: 'white' , padding: '8px 25px' , borderRadius: '10px' , '&:hover' : {bgcolor: '#7F56DA'} , width: '120px'}}>Search</Button>}
+            placeholder='Enter your email' disableUnderline
+                    ></Input>
+                </Grid>
+
+                <Grid margin={'50px 10px'} item>
+                    <img src={TSP} alt='' width={'90px'} height={'90px'} style={{borderRadius: '50%'}}></img>
+                </Grid>
+
+            </Grid>
+        {/* </Container> */}
+
     </>
   )
 }
