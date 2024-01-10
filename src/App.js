caa02ab9417138073fc04a53c72158aef41c81ad
Varrshinie123 <100944990+Varrshinie123@users.noreply.github.com>
2024-01-10 07:04:47 +0530

    Installed Google analyrics ,added measurement id in app.js and function to track page views
    Google analytics installed-reflected in changes in packcage.json
    React-ga installed and imported and used in app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import ReactGA from 'react-ga';
 import { Box, Stack, Switch } from '@mui/material';
 import './App.css';
 import Navbar from './Components/Navbar';
@@ -11,35 +14,37 @@ import MarketSavvyPage from './Components/MarketSavvy';
 import StockGyrationsPage from './Components/StockGyrations';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
 
+// Replace 'Measurement_Id' with your actual Measurement ID
+ReactGA.initialize('G-HLTBP4WRFD');
+
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    ReactGA.pageview(location.pathname + location.search);
+  }, [location]);
+
   return (
-   <>
-   <Box>
+    <>
+      <Box>
+        <Navbar />
+        <Stack direction={'column'} spacing={5}>
+          <Routes>
+            <Route exact path="/" element={<Services />} />
+          </Routes>
+        </Stack>
 
-    <Navbar/>
-    
-    <Stack direction={'column'} spacing={5}>
-
-      <Routes>
-      <Route exact path="/" element= {<Services/>} />
-      </Routes>
-
-    </Stack>
-
-    <Stack direction={'column'} spacing={5}>
-    <Routes>
-
-          <Route path="/OurTeam" element= {<OurTeam/>} />
-          <Route path="/DragonTalksPage" element= {<DragonTalksPage/>} />
-          <Route path="/StockGyrationsPage" element= {<StockGyrationsPage/>} />
-          <Route path="/MarketSavvyPage" element= {<MarketSavvyPage/>}  />
-          <Route path="/Contact" element= {<Contact/>}  />
-      
-    </Routes>
-    </Stack>
-
-   </Box>
-   </>
+        <Stack direction={'column'} spacing={5}>
+          <Routes>
+            <Route path="/OurTeam" element={<OurTeam />} />
+            <Route path="/DragonTalksPage" element={<DragonTalksPage />} />
+            <Route path="/StockGyrationsPage" element={<StockGyrationsPage />} />
+            <Route path="/MarketSavvyPage" element={<MarketSavvyPage />} />
+            <Route path="/Contact" element={<Contact />} />
+          </Routes>
+        </Stack>
+      </Box>
+    </>
   );
 }
 
